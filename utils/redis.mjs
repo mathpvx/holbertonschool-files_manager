63ef3bac9d3f865c0de46b202bfcde3384927f4a
mathpvx <7547@holbertonstudents.com>
2025-06-27 10:41:41 +0200

    older redis

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,42 +1,42 @@
-const redis = require('redis');
+import redis from 'redis';
 
 class RedisClient {
   constructor() {
-    this.client = redis.createClient();
-    this.client.on('error', (error) => console.error(`Redis client error: ${error}`));
+    this._client = redis.createClient();
+    this._client.on('error', (err) => console.error('Redis error:', err));
   }
 
   isAlive() {
-    return this.client.connected;
+    return this._client && this._client.connected;
   }
 
-  async get(key) {
+  get(key) {
     return new Promise((resolve, reject) => {
-      this.client.get(key, (err, reply) => {
+      this._client.get(key, (err, reply) => {
         if (err) reject(err);
-        else resolve(reply);
+        resolve(reply);
       });
     });
   }
 
-  async set(key, value, duration) {
+  set(key, value, duration) {
     return new Promise((resolve, reject) => {
-      this.client.setex(key, duration, value, (err, reply) => {
-        if (err) reject(err);
-        else resolve(reply);
+      this._client.setex(key, duration, String(value), (err) => {
+        if (err) return reject(err);
+        return resolve();
       });
     });
   }
 
-  async del(key) {
+  del(key) {
     return new Promise((resolve, reject) => {
-      this.client.del(key, (err, reply) => {
-        if (err) reject(err);
-        else resolve(reply);
+      this._client.del(key, (err) => {
+        if (err) return reject(err);
+        return resolve();
       });
     });
   }
 }
 
 const redisClient = new RedisClient();
-module.exports = redisClient;
+export default redisClient;
